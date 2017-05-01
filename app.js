5562c22b7cb1e7c1f8d0aa28d7c579a5350a7b4c
rabGIT <rabGIT@users.noreply.github.com>
2017-05-01 11:36:53 -0700

    parse and log form submission

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
+const commitFeed = require('./data/commits.json');
+var commits = JSON.stringify(commitFeed, null, 2);
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+
+  // process form request
+  var request = url.parse(req.url, true);
+  console.log(`Username: ${request.query.user} \nRepo: ${request.query.repo} `);
+
+  // build response
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end(fs.readFileSync('./public/index.html'));
+  var html = fs.readFileSync('./public/index.html');
+  html = html.toString().replace('{{ commitFeed }}' , commits)
+  res.end(html);
 });
 
 server.listen(port, hostname, () => {
